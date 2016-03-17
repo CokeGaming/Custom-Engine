efbb050cdf39fbe71469123d02734b7f8172c926
CokeGaming <Derek464646@yahoo.com>
2016-03-17 19:17:32 -0400

    Added files via upload

---
+++ CokeEngine/CokeEngine/WinMain.cpp
@@ -0,0 +1,21 @@
+#include "WinMain.h"	
+//Additional include files
+// #ifindef _String.H
+//#include "string.h_
+//#endif
+//ifindef _IOSTREAM.H
+    //#include "iostream.h"
+//#endif
+
+#if defined (DEBUG) | defined(_DEBUG)
+int _tmain(int argc, char* argv[])
+{
+
+
+}
+#endif
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+
+}
